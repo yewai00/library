53b178fa698bc8ff593a771468d6510d4c0e16de
Ye Wai <yewai.cs@gmail.com>
2023-12-01 08:58:37 +0630

    [main] add validation

--- src/main/java/com/sampleApp/library/model/request/BookRequest.java
+++ src/main/java/com/sampleApp/library/model/request/BookRequest.java
@@ -2,15 +2,32 @@ package com.sampleApp.library.model.request;
 
 import com.sampleApp.library.enums.Ratings;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class BookRequest {
+    @NotEmpty(message = "Title is required field.")
+    @Size(max = 255)
     private String title;
+
+    @Size(max = 255)
     private String summary;
+
     private String coverImg;
+
+    @NotEmpty(message = "Genres is required.")
+    @Size(max = 255)
     private String generes;
+
+    @NotEmpty(message = "Author is required.")
+    @Size(max = 255)
     private String author;
+
+    @Size(max = 255)
     private String tag;
+
+    @NotEmpty(message = "Rating is required.")
     private Ratings rating;
 }
--- src/main/java/com/sampleApp/library/model/request/CommentRequest.java
+++ src/main/java/com/sampleApp/library/model/request/CommentRequest.java
@@ -1,9 +1,18 @@
 package com.sampleApp.library.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentRequest {
+    @Email
+    @NotEmpty(message = "Email is required.")
+    @Size(max = 255)
     private String email;
+
+    @NotEmpty(message = "Comment is required.")
+    @Size(max = 255)
     private String comment;
 }
